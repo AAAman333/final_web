810952e9f5233afdfafc32827c4fb5d15fc7c2ac
AAAman333 <moldiryergesh@gmail.com>
2026-02-11 16:56:55 +0500

    Update flowerRoutes.js

--- routes/flowerRoutes.js
+++ routes/flowerRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const ctrl = require('../controllers/flowerController');
-const { protect, restrictTo } = require('../middleware/auth');
+const { protect, restrictToAdmin } = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getOne);
-router.post('/', protect, restrictTo('admin'), ctrl.create);
-router.put('/:id', protect, restrictTo('admin'), ctrl.update);
-router.delete('/:id', protect, restrictTo('admin'), ctrl.delete);
+router.post('/', protect, restrictToAdmin, ctrl.create);
+router.put('/:id', protect, restrictToAdmin, ctrl.update);
+router.delete('/:id', protect, restrictToAdmin, ctrl.delete);
 
 module.exports = router;
