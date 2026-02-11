0560fbe4becc01856fabc65226cdea38574509da
AAAman333 <moldiryergesh@gmail.com>
2026-02-11 17:02:55 +0500

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const ctrl = require('../controllers/orderController');
-const { protect, restrictTo } = require('../middleware/auth');
+const { protect, restrictToAdmin } = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/', protect, restrictTo('admin'), ctrl.getAll);
-router.get('/:id', protect, restrictTo('admin'), ctrl.getOne);
+router.get('/', protect, restrictToAdmin, ctrl.getAll);
+router.get('/:id', protect, restrictToAdmin, ctrl.getOne);
 router.post('/', protect, ctrl.create);
-router.put('/:id', protect, restrictTo('admin'), ctrl.update);
-router.delete('/:id', protect, restrictTo('admin'), ctrl.delete);
+router.put('/:id', protect, restrictToAdmin, ctrl.update);
+router.delete('/:id', protect, restrictToAdmin, ctrl.delete);
 
 module.exports = router;
